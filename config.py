22e558470055338ec8b8d92da8e2757bb2d86185
MrH4CK3474 <143846059+MrH4CK3474@users.noreply.github.com>
2024-03-18 13:10:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
     BOT_TOKEN = ""
-    SUPPORT_GROUP_LINK = "-1002021194325"
-    LOGS_GROUP_LINK = "https://t.me/CHATTING_2024"
+    SUPPORT_GROUP_LINK = "-1001800574833"
+    LOGS_GROUP_LINK = "https://t.me/COPYRIGHT_PROTECTOR"
     OWNER_ID = "6201525463"
     COPYRIGHT_MESSAGES = ["message1", "message2", "message3"]
